beb20039370e7eb155119f3a3746bf65bb1b7734
Westerbly Snaydley <westerbly@gmail.com>
2025-05-13 15:25:10 -0500

    move commands to script

---
+++ build.fish
@@ -0,0 +1,35 @@
+#!/usr/bin/env fish
+
+set os_name $argv[1]
+set build_script build.sh
+if test $os_name = "windows"
+    set build_script build_mingw.sh
+end
+
+function package_linux -a name
+    tar -czf "$name.tar.gz" --exclude "bin/eo" bin/ doc/ include/ lib/ testing/ CREDITS.md LICENSE.md
+end
+
+function package_windows -a name
+    zip -r "$name.zip" bin/ doc/ include/ lib/ testing/ CREDITS.md LICENSE.md -x "./bin/eo" "./bin/genie.exe"
+end
+
+function artifact -a branch
+    set -l revision (hg identify --template '{id|short}')
+    set artifact_name "scopes-unstable-$os_name-(date '+%Y-%m-%d')-$revision-$branch"
+    echo "artifact-name-$branch=$artifact_name" >> $GITHUB_OUTPUT
+
+    hg checkout $branch
+    # patch genie recipe
+    cp -f ../workarounds/genie.eo ./external/recipes/genie.eo
+    yes | ./$build_script --silent-progress
+
+    if test $os_name = "windows"
+        package_windows $artifact_name
+    else
+        package_linux $artifact_name
+    end
+end
+
+artifact default
+artifact based
